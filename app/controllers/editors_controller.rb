e5d7fb372dc731599a60c462e850394546d85fa0
Erick Smith <smith.s.erick@gmail.com>
2017-12-20 10:57:39 -0800

    Added devise auth_user

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -1,8 +1,8 @@
 class EditorsController < ApplicationController
   def index
-    unless user_signed_in?
-      redirect_to new_user_registration_path
-    end
+    # unless user_signed_in?
+    #   redirect_to new_user_registration_path
+    # end
     
   end
 end
