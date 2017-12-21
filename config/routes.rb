c036e5af52ff91ed88597f18ae830662ae108bac
Michael Topmiller <michaeltopmiller@gmail.com>
2017-12-21 01:15:16 +0000

    video_working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :vrooms
   match "/party/:id", :to => 'vrooms#party', :as => :party, :via => :get
 
-  # devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
