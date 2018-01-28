b32036670bb24d9407e57404dc7d661d36e95ee2
Erick Smith <smith.s.erick@gmail.com>
2018-01-27 16:50:49 -0800

    removed vroom model

--- app/assets/javascripts/add_room.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#special").on("submit", "form", function() {
-        $.post($(this).attr("action"), $(this).serialize(), function(response){
-            $("#new_chat").html(response)
-            console.log(response)
-        }, 'html')
-        return false;
-    })
-}) 
